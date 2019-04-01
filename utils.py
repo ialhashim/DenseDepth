9561be751b9b8d2cbfd6f2cda4895340f9a3d8c0
Ibraheem Alhashim <ibraheem.alhashim@gmail.com>
2019-04-01 16:52:53 +0300

    Added missing test time augmentation

--- utils.py
+++ utils.py
@@ -122,13 +122,18 @@ def evaluate(model, rgb, depth, crop, batch_size=6, verbose=True):
         true_y = depth[(i)*bs:(i+1)*bs,:,:]
         pred_y = scale_up(2, predict(model, x/255, minDepth=10, maxDepth=1000, batch_size=bs)[:,:,:,0]) * 10.0
         
+        # Test time augmentation: mirror image estimate
+        pred_y_flip = scale_up(2, predict(model, x[...,::-1,:]/255, minDepth=10, maxDepth=1000, batch_size=bs)[:,:,:,0]) * 10.0
+
         # Crop based on Eigen et al. crop
         true_y = true_y[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
         pred_y = pred_y[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
+        pred_y_flip = pred_y_flip[:,crop[0]:crop[1]+1, crop[2]:crop[3]+1]
         
         # Compute errors per image in batch
         for j in range(len(true_y)):
-            errors = compute_errors(true_y[j], pred_y[j])
+            errors = compute_errors(true_y[j], (0.5 * pred_y[j]) + (0.5 * np.fliplr(pred_y_flip[j])))
+            
             for k in range(len(errors)):
                 depth_scores[k][(i*bs)+j] = errors[k]
 
