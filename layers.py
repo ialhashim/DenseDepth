7c296c8f3aaa3dff869db3ab80abdc57d5b08789
Arijit Gupta <arijitgupta2000@gmail.com>
2020-09-28 20:26:28 +0530

    Update layers.py

--- layers.py
+++ layers.py
@@ -3,10 +3,21 @@ import keras.utils.conv_utils as conv_utils
 import tensorflow as tf
 import keras.backend as K
 
+def normalize_data_format(value):
+    if value is None:
+        value = K.image_data_format()
+    data_format = value.lower()
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('The `data_format` argument must be one of '
+                         '"channels_first", "channels_last". Received: ' +
+                         str(value))
+    return data_format
+    
+
 class BilinearUpSampling2D(Layer):
     def __init__(self, size=(2, 2), data_format=None, **kwargs):
         super(BilinearUpSampling2D, self).__init__(**kwargs)
-        self.data_format = K.normalize_data_format(data_format)
+        self.data_format = normalize_data_format(data_format)
         self.size = conv_utils.normalize_tuple(size, 2, 'size')
         self.input_spec = InputSpec(ndim=4)
 
@@ -35,7 +46,7 @@ class BilinearUpSampling2D(Layer):
             height = self.size[0] * input_shape[1] if input_shape[1] is not None else None
             width = self.size[1] * input_shape[2] if input_shape[2] is not None else None
         
-        return tf.image.resize_images(inputs, [height, width], method=tf.image.ResizeMethod.BILINEAR, align_corners=True)
+        return tf.image.resize(inputs, [height, width], method=tf.image.ResizeMethod.BILINEAR)
 
     def get_config(self):
         config = {'size': self.size, 'data_format': self.data_format}
