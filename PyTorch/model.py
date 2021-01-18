0ee04f7a536fed0a7c03a58c3c6e6d80490c1c0b
Yan <KevinYan37@163.com>
2021-01-19 00:14:43 +0800

    fix bug in pytorch model

--- PyTorch/model.py
+++ PyTorch/model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
+from torchvision import models
 import torch.nn.functional as F
+
 
 class UpSample(nn.Sequential):
     def __init__(self, skip_input, output_features):
@@ -12,25 +14,26 @@ class UpSample(nn.Sequential):
 
     def forward(self, x, concat_with):
         up_x = F.interpolate(x, size=[concat_with.size(2), concat_with.size(3)], mode='bilinear', align_corners=True)
-        return self.leakyreluB( self.convB( self.leakyreluA(self.convA( torch.cat([up_x, concat_with], dim=1) ) ) )  )
+        return self.leakyreluB( self.convB( self.convA( torch.cat([up_x, concat_with], dim=1)  ) )  )
 
 class Decoder(nn.Module):
-    def __init__(self, num_features=2208, decoder_width = 0.5):
+    def __init__(self, num_features=1664, decoder_width = 1.0):
         super(Decoder, self).__init__()
         features = int(num_features * decoder_width)
 
-        self.conv2 = nn.Conv2d(num_features, features, kernel_size=1, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(num_features, features, kernel_size=1, stride=1, padding=0)
 
-        self.up1 = UpSample(skip_input=features//1 + 384, output_features=features//2)
-        self.up2 = UpSample(skip_input=features//2 + 192, output_features=features//4)
-        self.up3 = UpSample(skip_input=features//4 +  96, output_features=features//8)
-        self.up4 = UpSample(skip_input=features//8 +  96, output_features=features//16)
+        self.up1 = UpSample(skip_input=features//1 + 256, output_features=features//2)
+        self.up2 = UpSample(skip_input=features//2 + 128,  output_features=features//4)
+        self.up3 = UpSample(skip_input=features//4 + 64,  output_features=features//8)
+        self.up4 = UpSample(skip_input=features//8 + 64,  output_features=features//16)
 
         self.conv3 = nn.Conv2d(features//16, 1, kernel_size=3, stride=1, padding=1)
 
     def forward(self, features):
-        x_block0, x_block1, x_block2, x_block3, x_block4 = features[3], features[4], features[6], features[8], features[11]
-        x_d0 = self.conv2(x_block4)
+        x_block0, x_block1, x_block2, x_block3, x_block4 = features[3], features[4], features[6], features[8], features[12]
+        x_d0 = self.conv2(F.relu(x_block4))
+
         x_d1 = self.up1(x_d0, x_block3)
         x_d2 = self.up2(x_d1, x_block2)
         x_d3 = self.up3(x_d2, x_block1)
@@ -40,8 +43,7 @@ class Decoder(nn.Module):
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()       
-        import torchvision.models as models
-        self.original_model = models.densenet161( pretrained=True )
+        self.original_model = models.densenet169( pretrained=False )
 
     def forward(self, x):
         features = [x]
